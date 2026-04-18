14f2cf6243c7785fca8e8ae88a7fff2c4cb42a4f
Ashish Saxena <ashish.sxn847@gmail.com>
2026-04-18 15:17:24 +0530

    Mac 0S nav done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './app.scss'
+import Dock from './components/Dock'
+import Nav from './components/Nav'
 
 function App() {
 
   return (
     <main>
-      
+      <Nav/>
+      <Dock/>
     </main>
   )
 }
